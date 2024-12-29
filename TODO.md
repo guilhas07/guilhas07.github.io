4fbfde33b8eff9391dc40708d12192cfebe74ff6
Guilherme Soares <guilhermesoares1970@gmail.com>
2024-12-29 04:33:46 +0000

    feat: add button to toggle sidebar. Collapse sidebar under lg breakpoint
    Additional Changes:
    - fix: don't trigger animations when toggling between breakpoints

--- TODO.md
+++ TODO.md
@@ -1,4 +1,5 @@
-Add hide button for sidebarinfo. Keep sidebar hidden by default under lg breakpoints. - [ ]
+Add hide button for sidebarinfo. Keep sidebar hidden by default under lg breakpoints. - [x]
 Fix bootstrap collapse (it isn't smooth when hiding) - [ ]
 Add dynamic font size to Table of Contents to prevent overflow - [ ]
 On section pages the date seems to close to the title on the card-bodys. Only on the github deployment though... - [ ]
+grow left border under lg breakpoints
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,44 @@
 const currentPage = window.location.pathname;
+const BREAKPOINT_LG = 991.98;
 let animation = sessionStorage.getItem("animation") === "true";
+let showSideBar = localStorage.getItem("showSideBar") === "true";
+let sidebar = document.querySelector(".my-sidebar");
+let toggler = document.getElementById("sidebar-toggler");
+toggler.addEventListener("click", toggle);
+window.addEventListener("resize", hideAnimations());
 
-function hide() {
-    let sidebar = document.querySelector(".my-side-bar");
-    // switching to home/resume page (toggle off the sidebar with animation)
-    if (animation) {
-        sidebar.classList.add("show");
-        void document.body.offsetHeight;
+switch (currentPage) {
+    case "/":
+    case "/resume/":
+        hideToggler();
+        if (animation) {
+            sidebar.classList.add("show");
+            toggler.classList.add("show");
+            void document.body.offsetHeight;
 
-        document.body.classList.remove("preload");
-        sidebar.classList.remove("show");
-        sessionStorage.setItem("animation", false);
-    } else {
-        document.body.classList.remove("preload");
-    }
-}
-
-function show() {
-    let sidebar = document.querySelector(".my-side-bar");
-    if (animation) {
-        sidebar.classList.add("show");
-        void document.body.offsetHeight;
-        document.body.classList.remove("preload");
-    } else {
-        document.body.classList.remove("preload");
-        sidebar.classList.add("show");
-        sessionStorage.setItem("animation", true);
-    }
-}
-
-window.onload = function () {
-    switch (currentPage) {
-        case "/":
-        case "/resume/":
+            document.body.classList.remove("preload");
             hide();
-            break;
-        default:
-            show();
-            break;
-    }
-};
+        } else {
+            document.body.classList.remove("preload");
+        }
+        break;
+    default:
+        if (showSideBar && window.innerWidth > BREAKPOINT_LG) {
+            if (animation) {
+                sidebar.classList.add("show");
+                toggler.classList.add("show");
+                void document.body.offsetHeight;
+            } else {
+                document.body.classList.remove("preload");
+                show();
+            }
+        }
+        document.body.classList.remove("preload");
+        break;
+}
 
-let previousActive = null;
+// Table of Content active code
+let activeElement = null;
 const observer = new IntersectionObserver((entries) => {
     for (entry of entries) {
         // NOTE: intersection ratios are only 1 or 0
@@ -54,7 +51,7 @@ const observer = new IntersectionObserver((entries) => {
         const element = links.get(key);
         if (!found && val) {
             found = true;
-            previousActive = key;
+            activeElement = key;
             element.classList.add("active");
             history.replaceState(null, null, `#${key}`);
         } else {
@@ -62,10 +59,7 @@ const observer = new IntersectionObserver((entries) => {
         }
     });
 
-    // Activate the previousActive
-    if (!found) {
-        links.get(previousActive).classList.add("active");
-    }
+    links.get(activeElement).classList.add("active");
 });
 
 let links = new Map();
@@ -75,4 +69,49 @@ for (el of document.querySelectorAll("#TableOfContents a[href]")) {
     links.set(id, el);
     actives.set(id, false);
     observer.observe(document.getElementById(id));
+}
+
+function toggle() {
+    // use localStorage so user preference persists between sessions
+    if (sidebar.classList.contains("show")) {
+        hide();
+        localStorage.setItem("showSideBar", false);
+    } else {
+        show();
+        localStorage.setItem("showSideBar", true);
+    }
+}
+
+function hide() {
+    sidebar.classList.remove("show");
+    toggler.classList.remove("show");
+    sessionStorage.setItem("animation", false);
+}
+
+function show() {
+    sidebar.classList.add("show");
+    toggler.classList.add("show");
+    sessionStorage.setItem("animation", true);
+}
+
+function hideToggler() {
+    toggler.style.display = "none";
+}
+
+function hideAnimations() {
+    let previousWidth = document.body.clientWidth;
+    return () => {
+        let currentWidth = document.body.clientWidth;
+
+        let newDiff = currentWidth - BREAKPOINT_LG;
+        let previousDiff = previousWidth - BREAKPOINT_LG;
+
+        // In between breakpoints. Disable animations
+        if (previousDiff > 0 != newDiff > 0) {
+            document.body.classList.add("noanimation");
+            void document.body.offsetHeight;
+            document.body.classList.remove("noanimation");
+        }
+        previousWidth = currentWidth;
+    };
 }
